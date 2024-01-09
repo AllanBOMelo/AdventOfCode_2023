9d8eb016644019d5f8c910a06d22436b09ca9c5a
Allan Barbosa <115508413+AllanBOMelo@users.noreply.github.com>
2024-01-09 13:24:25 +0000

    ⚙ Initial Commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+import { BrowserRouter } from "react-router-dom"
+
+const Router: any = () => {
+    return (
+        <BrowserRouter>
+            <Fragment>
+                
+            </Fragment>
+        </BrowserRouter>
+    )
+}
+
+
+export default Router
